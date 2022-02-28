5c2810db845aa13d3be4bff9c68b584bfe15ba3f
liuzhe <liuzhe02@zuoyebang.com>
2022-03-01 02:42:01 +0800

    feat: 内容安全策略

--- server.js
+++ server.js
@@ -2,19 +2,24 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(request, response) {
+    console.log('request come', request.url);
     if (request.url === '/') {
-        console.log('request come', request.url);
         const html = fs.readFileSync('test.html', 'utf8');
         response.writeHead(200, {
             'Content-Type': 'text/html',
+            // 'Content-Security-Policy': 'default-src http: https:', // 只能使用外链js
+            // 'Content-Security-Policy': 'default-src \'self\'' //只能使用本域名的外链js
+            // 'Content-Security-Policy': 'default-src \'self\' https://s.momocdn.com/' //只能使用本域名的外链js
+            // 'Content-Security-Policy': 'default-src \'self\'; report-uri /report', //不加载资源 && 报告错误
+            // 'Content-Security-Policy-Report-only': 'default-src \'self\'; report-uri /report' //只报告错误
+            
         })
         response.end(html);
     } else {
-        const jpg = fs.readFileSync('test.jpg');
         response.writeHead(200, {
-            'Content-Type': 'image/jpg',
-        });
-        response.end(jpg)
+            'Content-Type': 'application/javascript'
+        })
+        response.end('console.log("load url")')
     }
     
 }).listen(8888);
