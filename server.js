9fae5789ba844b0443e8baeea0fe97d0ef687831
liuzhe <liuzhe02@zuoyebang.com>
2022-03-01 02:03:52 +0800

    feat: 长连接

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(request, response) {
-    console.log('request come', request.url);
-    const html = fs.readFileSync('test.html', 'utf8');
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
-    response.end(html);
+    if (request.url === '/') {
+        console.log('request come', request.url);
+        const html = fs.readFileSync('test.html', 'utf8');
+        response.writeHead(200, {
+            'Content-Type': 'text/html',
+        })
+        response.end(html);
+    } else {
+        const jpg = fs.readFileSync('test.jpg');
+        response.writeHead(200, {
+            'Content-Type': 'image/jpg',
+        });
+        response.end(jpg)
+    }
+    
 }).listen(8888);
 
 
