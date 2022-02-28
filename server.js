34fb5724ba28d89ea097a5bd690af6e64d5e668c
liuzhe <liuzhe02@zuoyebang.com>
2022-02-28 23:28:08 +0800

    feat: 预请求

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(request, response) {
-    console.log('request come', request.url);
-    const html = fs.readFileSync('test.html', 'utf8');
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
-    response.end(html);
+    if (request.url === '/') {
+        console.log('request come', request.url);
+        const html = fs.readFileSync('test.html', 'utf8');
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        response.end(html);
+    } else {
+        response.end('server');
+    }
+    
 }).listen(8888);
 
 
--- server2.js
+++ server2.js
@@ -3,9 +3,11 @@ const fs = require('fs');
 
 http.createServer(function(request, response) {
     console.log('request come', request.url);
-    // response.writeHead(200, {
-    //     'Access-Control-Allow-Origin': '*'
-    // })
+    response.writeHead(200, {
+        'Access-Control-Allow-Origin': '*',
+        // 'Access-Control-Allow-Headers': 'X-Test-Cors',
+        // 'Access-Control-Allow-methods': 'PUT'
+    })
     response.end('123');
 }).listen(8887);
 
