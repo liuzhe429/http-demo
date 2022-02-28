e1b9623464c6773bea76f4dc2c8c08f10f961075
liuzhe <liuzhe02@zuoyebang.com>
2022-02-28 23:52:11 +0800

    feat: 预请求

--- server2.js
+++ server2.js
@@ -6,7 +6,8 @@ http.createServer(function(request, response) {
     response.writeHead(200, {
         'Access-Control-Allow-Origin': '*',
         // 'Access-Control-Allow-Headers': 'X-Test-Cors',
-        // 'Access-Control-Allow-methods': 'PUT'
+        // 'Access-Control-Allow-methods': 'PUT',
+        // 'Access-Control-Max-Age': '10'
     })
     response.end('123');
 }).listen(8887);
