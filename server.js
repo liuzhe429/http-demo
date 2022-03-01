d6508e7085f6a7acf6ec5f033c29975f4352f68d
liuzhe <liuzhe02@zuoyebang.com>
2022-03-01 17:45:24 +0800

    feat: 重定向

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(request, response) {
-    console.log('request come', request.url);
     if (request.url === '/') {
-        // 临时跳转 后续路由可能还会修改
+        console.log('request come', request.url);
+        // 302 临时跳转 后续路由可能还会修改
         // 301 永久重定向 慎重！！！！
         // 200 无用
-        response.writeHead(302, {
+        response.writeHead(301, {
             'Location': '/new'
         })
-        response.end();
+        response.end('123');
     }
     if (request.url === '/new') {
+        console.log('request come', request.url);
         response.writeHead(200, {
             'Content-Type': 'text/html'
         })
